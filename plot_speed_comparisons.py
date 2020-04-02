0c00df3a97f7d0471e7caac9194335fdbdf1a6cd
John Jasa <johnjasa11@gmail.com>
2020-04-02 12:24:08 -0600

    Checkpointing reasonable comparisons for num_inputs, num_outputs, and bandwidth

--- plot_speed_comparisons.py
+++ plot_speed_comparisons.py
@@ -4,31 +4,39 @@ from time import time
 import numpy as np
 from collections import OrderedDict
 import pickle
+import matplotlib.pyplot as plt
 
 import openmdao.api as om
 
 
-data = OrderedDict()
-data['Analytic Dense'] = None
-data['Analytic Sparse'] = None
-data['JAX Jacobian'] = None
-data['Approximated'] = None
-data['Approximated Colored'] = None
-
-with open('timing_data.pkl', 'rb') as f:
-    output_data = pickle.load(f)
-
-timing_data = output_data['timing_data']
-nns = output_data['nns']
-
-import matplotlib.pyplot as plt
-
-plt.figure()
-
-for i_method, key in enumerate(data):
-    plt.loglog(nns, timing_data[:, i_method], label=key)
+label_keys = {
+    'num_outputs' : 'Num outputs',
+    'num_inputs' : 'Num inputs',
+    'bandwidth' : 'Bandwidth',
+    }
     
-plt.legend()
-plt.xlabel('Num outputs')
-plt.ylabel('Time to compute total derivs, secs')
-plt.savefig('speed_comparison.pdf')
+for varied_term in label_keys:
+    filename = f'timing_data_{varied_term}.pkl'
+
+    data = OrderedDict()
+    data['Analytic Sparse'] = None
+    data['Analytic Dense'] = None
+    data['Approximated'] = None
+    data['Approximated Colored'] = None
+    data['JAX Jacobian'] = None
+
+    with open(filename, 'rb') as f:
+        output_data = pickle.load(f)
+
+    timing_data = output_data['timing_data']
+    nns = output_data['nns']
+
+    plt.figure()
+
+    for i_method, key in enumerate(data):
+        plt.loglog(nns, timing_data[:, i_method], label=key)
+        
+    plt.legend()
+    plt.xlabel(label_keys[varied_term])
+    plt.ylabel('Time to compute total derivs, secs')
+    plt.savefig(f'speed_comparison_{varied_term}.pdf')
--- run_speed_comparisons.py
+++ run_speed_comparisons.py
@@ -9,72 +9,101 @@ import openmdao.api as om
 from groups import MatrixGroup
 
 
-nns = [2**i for i in range(4, 14)]
-num_nns = len(nns)
-num_repeats = 10
+random_seed = 310
 
-data = OrderedDict()
-data['Analytic Dense'] = np.zeros((num_nns, num_repeats))
-data['Analytic Sparse'] = np.zeros((num_nns, num_repeats))
-data['JAX Jacobian'] = np.zeros((num_nns, num_repeats))
-data['Approximated'] = np.zeros((num_nns, num_repeats))
-data['Approximated Colored'] = np.zeros((num_nns, num_repeats))
-
-timing_data = np.zeros((num_nns, len(data)))
-
-for i_method, key in enumerate(data):
+keys = ['num_outputs', 'num_inputs', 'bandwidth']
+for varied_term in keys:
     
-    print()
-    print(i_method, key)    
-    
-    if key == 'Analytic Dense':
-        from matrix_comp_analytic import MatrixComp
-    if key == 'Analytic Sparse':
-        from matrix_comp_analytic_sparse import MatrixComp
-    if key == 'JAX Jacobian':
-        from matrix_comp_jax import MatrixComp
-    if key == 'Approximated':
-        from matrix_comp_approx import MatrixComp
-    if key == 'Approximated Colored':
-        from matrix_comp_approx_colored import MatrixComp
-        
-    for i_nn, num_outputs in enumerate(nns):
-        
-        if num_outputs > 100 and 'JAX' in key:
-            timing_data[i_nn, i_method] = np.nan
-            continue
-        
-        print('Running {} cases of {} num_outputs'.format(num_repeats, num_outputs))
-        
+    if 'output' in varied_term:
+        nns = [2**i for i in range(2, 12)]
         num_inputs = 5
-        # num_outputs = 3
+        num_outputs = 3
         bandwidth = 2
-        random_seed = 310
+    elif 'input' in varied_term:
+        nns = [2**i for i in range(2, 12)]
+        num_inputs = 5
+        num_outputs = 3
+        bandwidth = 2
+    elif 'bandwidth' in varied_term:
+        nns = [2**i for i in range(1, 6)]
+        num_inputs = 200
+        num_outputs = 100
+        bandwidth = 2            
 
-        prob = om.Problem()
-        prob.model = MatrixGroup(comp_type=MatrixComp, num_inputs=num_inputs, num_outputs=num_outputs, bandwidth=bandwidth, random_seed=random_seed)
-        
-        prob.setup()
-        prob.run_model()
-        
-        for i_repeat in range(num_repeats):
+    num_nns = len(nns)
+    num_repeats = 20
 
-            pre_time = time()
-            
-            prob.compute_totals(['obj'], ['x'])
-            
-            post_time = time()
-            duration = post_time - pre_time
-            
-            data[key][i_nn, i_repeat] = duration
-            
-        timing_data[i_nn, i_method] = np.mean(data[key][i_nn, :])
+    data = OrderedDict()
+    data['Analytic Sparse'] = np.zeros((num_nns, num_repeats))
+    data['Analytic Dense'] = np.zeros((num_nns, num_repeats))
+    data['Approximated'] = np.zeros((num_nns, num_repeats))
+    data['Approximated Colored'] = np.zeros((num_nns, num_repeats))
+    data['JAX Jacobian'] = np.zeros((num_nns, num_repeats))
+
+    timing_data = np.zeros((num_nns, len(data)))
+
+    for i_method, key in enumerate(data):
         
+        print()
+        print(i_method, key)    
         
-output_data = {
-    'timing_data' : timing_data,
-    'nns' : nns,
-    }
-    
-with open('timing_data.pkl', 'wb') as f:
-    pickle.dump(output_data, f)
+        if key == 'Analytic Dense':
+            from matrix_comp_analytic import MatrixComp
+        if key == 'Analytic Sparse':
+            from matrix_comp_analytic_sparse import MatrixComp
+        if key == 'JAX Jacobian':
+            from matrix_comp_jax import MatrixComp
+        if key == 'Approximated':
+            from matrix_comp_approx import MatrixComp
+        if key == 'Approximated Colored':
+            from matrix_comp_approx_colored import MatrixComp
+            
+        for i_nn, nn in enumerate(nns):
+            
+            if nn > 150 and 'JAX' in key:
+                timing_data[i_nn, i_method] = np.nan
+                continue
+            elif nn > 40 and 'JAX' in key and 'bandwidth' in varied_term:
+                timing_data[i_nn, i_method] = np.nan
+                continue
+            
+            if 'outputs' in varied_term:
+                num_outputs = nn
+            elif 'inputs' in varied_term:
+                num_inputs = nn
+            elif 'bandwidth' in varied_term:
+                bandwidth = nn
+                
+            print(f'Running {num_repeats} cases of {num_inputs} num_inputs, {num_outputs} num_outputs, {bandwidth} bandwidth')
+            
+            prob = om.Problem()
+            prob.model = MatrixGroup(comp_type=MatrixComp, num_inputs=num_inputs, num_outputs=num_outputs, bandwidth=bandwidth, random_seed=random_seed)
+            
+            prob.setup()
+            prob.run_model()
+            
+            for i_repeat in range(num_repeats):
+
+                pre_time = time()
+                
+                prob.compute_totals(['obj'], ['x'])
+                
+                post_time = time()
+                duration = post_time - pre_time
+                
+                data[key][i_nn, i_repeat] = duration
+                
+            timing_data[i_nn, i_method] = np.mean(data[key][i_nn, :])
+            
+            
+    output_data = {
+        'timing_data' : timing_data,
+        'nns' : nns,
+        'num_inputs' : num_inputs,
+        'num_outputs' : num_outputs,
+        'bandwidth' : bandwidth,
+        'varied_term' : varied_term,
+        }
+        
+    with open(f'timing_data_{varied_term}.pkl', 'wb') as f:
+        pickle.dump(output_data, f)
